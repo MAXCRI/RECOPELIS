d205d871af9768e7074c877a6b7d39f954e0e525
maxicristaldo <maximilianocristaldo@mail.com>
2023-04-11 22:09:45 -0300

    primeros pasos en ReactNative

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola soy Denise Tonta !!</Text>
       <StatusBar style="auto" />
     </View>
   );
